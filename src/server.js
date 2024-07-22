897fe6ab7f2c441e7ffe91687a38ab2667f9967b
vButkevich <vyacheslav.butkevich@gmail.com>
2024-07-22 12:33:13 +0300

    count

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ export const setupServer = () => {
     res.status(200).json({
       status: 200,
       message: 'Successfully found contacts!',
-      count: contacts.length,
+      // count: contacts.length,
       data: contacts,
     });
   });
