1ee17443cf14314202749f1e6b49e105aa2a6eb1
vButkevich <vyacheslav.butkevich@gmail.com>
2024-07-24 15:07:47 +0300

    pagination

--- src/services/contactsService.js
+++ src/services/contactsService.js
@@ -1,27 +1,21 @@
 import { ContactsCollection } from '../db/models/contactsModel.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getContactsService = async (page = 1, perPage = 10) => {
+export const getContactsService = async ({ page = 1, perPage = 10 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = await ContactsCollection.find();
-  const contactsCount = await ContactsCollection.find()
-    .merge(contactsQuery)
-    .countDocuments();
-
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
-
-  /*
-  const [contactCount, contacts] = await Promise.all([
+  const contactsQuery = ContactsCollection.find();
+  const [contactsCount, contacts] = await Promise.all([
     ContactsCollection.find().merge(contactsQuery).countDocuments(),
     contactsQuery
       .skip(skip)
       .limit(limit)
       .exec(),
   ]);
-  */
+
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
+  
   return {
     contacts: contacts,
     ...paginationData,
