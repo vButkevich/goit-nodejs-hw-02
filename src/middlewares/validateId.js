0a0fb26d00c5e9db153e277084a9e1576490fc14
vButkevich <vyacheslav.butkevich@gmail.com>
2024-07-25 14:29:56 +0300

    fix:validateId

--- src/middlewares/validateId.js
+++ src/middlewares/validateId.js
@@ -2,7 +2,9 @@ import { HttpError } from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 
 export const validateId = (req, res, next) => {
-  const { id } = req.params;
+  const params = req.params;
+  const id = params.contactId;
+  // const { id } = req.params;
   if (!isValidObjectId(id)) {
     throw HttpError(400, 'invalidIdObject: Bad request');
   }
