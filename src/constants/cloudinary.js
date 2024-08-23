c0e177c8dac2864c70340fe5775546c4cf822821
vButkevich <vyacheslav.butkevich@gmail.com>
2024-08-24 01:02:05 +0300

    hw6

--- src/constants/cloudinary.js
+++ src/constants/cloudinary.js
@@ -1,8 +1,8 @@
 import { env } from '../utils/env.js';
 
 export const CLOUDINARY = {
-  ENABLE: env('CLOUDINARY-ENABLE', 'false'),
-  CLOUD_NAME: env('CLOUDINARY-CLOUD_NAME'),
-  API_SECRET: env('CLOUDINARY-API_SECRET'),
-  API_KEY: env('CLOUDINARY-API_KEY'),
+  ENABLE: env('CLOUDINARY_ENABLE', 'false'),
+  CLOUD_NAME: env('CLOUDINARY_CLOUD_NAME'),
+  API_SECRET: env('CLOUDINARY_API_SECRET'),
+  API_KEY: env('CLOUDINARY_API_KEY'),
 };
