48bc7d8cfedd897ac871c45cdf8b9b481f6ccdeb
vButkevich <vyacheslav.butkevich@gmail.com>
2024-08-25 01:37:01 +0300

    hw7.2:swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,9 @@
 // src/constants/index.js
 
 import path from 'node:path';
+// import { env } from './env.js';
 
-export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+// export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 export const UPLOADS_DIR = path.join(process.cwd(), 'uploads');
 export const TEMP_DIR = path.join(process.cwd(), 'temp');
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,10 +1,11 @@
 // src/routers/index.js
 
 import { Router } from 'express';
-import authUserRouter from './authUserRouter.js';
-import authGoogleRouter from './authGoogleRouter.js';
-import contactsRouter from './contactsRouter.js';
 import { auth } from '../middlewares/auth.js';
+import { swaggerDocs } from '../middlewares/swaggerDocs.js';
+import authGoogleRouter from './authGoogleRouter.js';
+import authUserRouter from './authUserRouter.js';
+import contactsRouter from './contactsRouter.js';
 
 const router = Router();
 
@@ -17,5 +18,8 @@ router.use('/auth', authUserRouter);
 router.use('/auth/google', authGoogleRouter);
 router.use('/contacts/all', contactsRouter);
 router.use('/contacts', auth, contactsRouter);
+router.use('/contacts', auth, contactsRouter);
+router.use('/api-docs', swaggerDocs());
+
 
 export default router;
